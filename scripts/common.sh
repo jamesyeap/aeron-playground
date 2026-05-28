bcd8192b2833214b20f997e24f71428eb0227ca0
James Yeap <jamesyeap.ys@gmail.com>
2026-05-28 10:35:56 +0800

    feat: add standalone media driver

---
+++ scripts/common.sh
@@ -0,0 +1,3 @@
+
+ADD_OPENS="--add-opens java.base/jdk.internal.misc=ALL-UNNAMED --add-opens java.base/java.util.zip=ALL-UNNAMED"
+AERON_LIB_JAR="/Users/jamesyeap/.gradle/caches/modules-2/files-2.1/io.aeron/aeron-all/1.51.0/4d17308cba9d4ff3ff97833d6dac52e3289f81e1/aeron-all-1.51.0.jar"
---
+++ src/main/java/StartMediaDriver.java
@@ -0,0 +1,34 @@
+import io.aeron.driver.MediaDriver;
+import io.aeron.driver.ThreadingMode;
+import org.agrona.concurrent.BusySpinIdleStrategy;
+
+/**
+ * StartMediaDriver sets up the MediaDriver that will be used by all applications on this box to send and receive messages.
+ * <p>
+ * We're running the MediaDriver as a stand-alone process - although it can also be embedded into the same process as one of the applications.
+ */
+public class StartMediaDriver {
+    public static void main(String[] args) {
+        // specify this with example below:
+        //  -DaeronPlayground.dir=/tmp/media-driver-1
+        String aeronDir = System.getProperty("aeronPlayground.dir");
+
+        // create the config for the media driver
+        final MediaDriver.Context mediaDriverCtx = new MediaDriver.Context()
+                .aeronDirectoryName(aeronDir)
+                .sharedIdleStrategy(new BusySpinIdleStrategy())
+                .threadingMode(ThreadingMode.SHARED);
+
+        // start the media driver - note: we use the "try-with" pattern here, so that
+        // the `close()` method of the MediaDriver can automatically run after this block is exited - see `AutoClosable`
+        try (final MediaDriver mediaDriver = MediaDriver.launch(mediaDriverCtx)) {
+            System.out.format("Media driver started! Aeron directory: %s\n", aeronDir);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.format("Shutting down media driver - Aeron directory: %s\n", aeronDir)));
+
+            // to prevent shutdown
+            while (true) {
+            }
+        }
+    }
+}
