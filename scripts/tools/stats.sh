107a77163338be2a82d6c08da9462ce5482a117c
James Yeap <jamesyeap.ys@gmail.com>
2026-05-29 09:44:35 +0800

    fix: add more options for stats

--- scripts/tools/stats.sh
+++ scripts/tools/stats.sh
@@ -1,12 +1,20 @@
-# === CONFIG ===
-AERON_DIR="/tmp/media-driver-1"
+# === OPTIONS ===
+AERON_STAT="io.aeron.samples.AeronStat"
+ERROR_STAT=io.aeron.samples.ErrorStat
+STREAM_STAT=io.aeron.samples.StreamStat
+BACKLOG_STAT=io.aeron.samples.BacklogStat
+LOSS_STAT=io.aeron.samples.LossStat
 
-# === CONSTANTS ===
+# === JVM STUFF ===
 AERON_JAR="/Users/jamesyeap/Developer/java/aeron/aeron-all/build/libs/aeron-all-1.51.0.jar"
 ADD_OPENS="--add-opens java.base/jdk.internal.misc=ALL-UNNAMED"
+
+# === CONFIG ===
+AERON_DIR="/tmp/media-driver-1"
+PROGRAM=${AERON_STAT}
 
 java \
   -cp ${AERON_JAR} \
   ${ADD_OPENS} \
   -Daeron.dir=${AERON_DIR} \
-  io.aeron.samples.StreamStat
+  ${PROGRAM}
